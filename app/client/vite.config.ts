ad24de38844402b31fdf10e516d263f49ec0a0d8
egulliver1721 <118499423+egulliver1721@users.noreply.github.com>
2023-04-17 16:36:01 +0800

    Initial commit

---
+++ app/client/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    include: ["react/jsx-runtime", "react/jsx-dev-runtime", "react"],
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
